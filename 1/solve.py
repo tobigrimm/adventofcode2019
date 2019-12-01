47aa43d96fd4dfb44c9f868ef94ae6fe51ace0c6
Tobi <tobi-git@grimm.gr>
2019-12-01 13:59:19 +0100

    Day01 done \o/

--- 1/solve.py
+++ 1/solve.py
@@ -10,6 +10,25 @@ def part_1(input):
             sum += calc_module(int(line.strip()))
     return sum
 
+def recurse_calc_fuel(weight):
+    new_weight = calc_module(weight)
+    if new_weight <= 0:
+        return weight
+    else:
+        additional_fuel = recurse_calc_fuel(new_weight)
+        return weight + additional_fuel
+
+
+def part_2(input):
+    """recursively calculate and add fuel,
+    until the remaining weight is <=0"""
+    sum = 0
+    for line in input:
+        # here might be a good spot for the 3.8 walrus operator :)
+        if len(line.strip())>0:
+            #print(line)
+            sum += recurse_calc_fuel(calc_module(int(line.strip())))
+    return sum
 
 
 
@@ -18,3 +37,5 @@ if __name__ == "__main__":
         input_1 = inputfile.readlines()
     print("part 1")
     print(part_1(input_1))
+    print("part 2")
+    print(part_2(input_1))
