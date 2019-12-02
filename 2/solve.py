2f852265e6b11cf25157be98f79f6de4191253e4
Tobi <tobi-git@grimm.gr>
2019-12-02 09:42:49 +0100

    some more flexibility for part2

--- 2/solve.py
+++ 2/solve.py
@@ -1,23 +1,29 @@
-instructions = []
 
 with open("input") as inputfile:
     instructions = [int(i) for i in inputfile.readline().strip().split(",")]
+
+def calculate_output(memory):
+    IP = 0
+    instructions = memory
+
+    while instructions[IP] != 99:
+        if instructions[IP] == 1:
+            instructions[instructions[IP+3]] = instructions[instructions[IP+1]] + instructions[instructions[IP+2]]
+        if instructions[IP] == 2:
+            instructions[instructions[IP+3]] = instructions[instructions[IP+1]] * instructions[instructions[IP+2]]
+            
+        # print(cur, instructions[cur])
+        IP +=4
+
+    #print("Value at adress 0 after running: %s" % instructions[0])
+    return instructions[0]
+
+# part1
 
 #restore previous state:
 
 instructions[1] = 12
 instructions[2] = 2
 
-cur = 0
-
-while instructions[cur] != 99:
-    if instructions[cur] == 1:
-        instructions[instructions[cur+3]] = instructions[instructions[cur+1]] + instructions[instructions[cur+2]]
-    if instructions[cur] == 2:
-        instructions[instructions[cur+3]] = instructions[instructions[cur+1]] * instructions[instructions[cur+2]]
-        
-    # print(cur, instructions[cur])
-    cur +=4
-
-print("Value at adress 0 after running: %s" % instructions[0])
+print("Value at adress 0 after running: %s" % calculate_output(instructions))
 
