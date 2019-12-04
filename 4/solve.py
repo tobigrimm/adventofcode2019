1b69f3e0024d3c896a1eb1b1d3c510f8a493f5ce
Tobi <tobi-git@grimm.gr>
2019-12-04 08:41:00 +0100

    and part2 done

--- 4/solve.py
+++ 4/solve.py
@@ -15,9 +15,21 @@ def check_pw(password):
             return False
     return True
 
+def check_pw_part2(password):
+    password = str(password)
+    if password[0] == password[1] != password[2] or password[0] != password[1] == password[2] != password[3] or password[1] != password[2] == password[3] != password[4] or password[2] != password[3] == password[4] != password [5] or password[3] != password[4] == password[5]:
+        pass
+    else:
+        return False
+    return True
+
 result = []
 for p in range(183564,657474+1):
     result.append(check_pw(p))
 
-print(result.count(True))
-print(sum(result))
+print("Part 1: ",result.count(True))
+
+result = []
+for p in range(183564,657474+1):
+    result.append(check_pw(p) and check_pw_part2(p))
+print("Part 2: ",result.count(True))
