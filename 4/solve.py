465406bcd1d6a35ca8fd9f0238ad8ea715b94f60
Tobi <tobi-git@grimm.gr>
2019-12-04 08:37:46 +0100

    this solves day04 - part1

---
+++ 4/solve.py
@@ -0,0 +1,23 @@
+def check_pw(password):
+    password = str(password)
+    #if not len(password)==6:
+    #    return false
+    if password[0] == password[1] or password[1] == password[2] or password[2] == password[3] or password[3] == password[4] or password[4] == password[5]:
+        pass
+    else:
+        return False
+    
+    last_digit = 0
+    for i in range(len(password)):
+        if password[i]>=last_digit:
+            last_digit = password[i]
+        else:
+            return False
+    return True
+
+result = []
+for p in range(183564,657474+1):
+    result.append(check_pw(p))
+
+print(result.count(True))
+print(sum(result))
